d2db1ddc691f84b0fdf789b14b4964fd39aa7338
crn2021 <92285177+crn2021@users.noreply.github.com>
2021-10-20 11:12:08 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "cross":return img`
+5 . . . . . . . . . . . . . . 4 
+5 5 . . . . . . . . . . . . 4 . 
+. 5 5 . . . . . . . . . . 4 . . 
+. . 5 5 . . . 2 2 . . . 4 . . . 
+. 7 . 5 5 . . 2 2 . . 4 . . . . 
+. 7 . . 5 5 . 2 2 . 4 . . . 4 . 
+. 7 . . . 5 5 2 2 4 4 . . . 4 . 
+. 7 . . . . 5 5 8 4 4 . . . 4 . 
+. . . . . 4 4 8 5 . . . . 4 4 . 
+. . . . 4 4 4 2 2 5 . . . . . . 
+. . . . 4 4 . 2 2 5 5 . . . . . 
+. . . 4 4 . . 2 2 . 5 5 . . . . 
+. . 4 4 . . . 2 2 . . 5 5 . . . 
+4 4 4 . . . . . . . . . 5 5 . . 
+4 4 . . . . . . . . . . . 5 5 . 
+4 . . . . . . . . . . . . . 5 5 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
